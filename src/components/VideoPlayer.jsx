9c6924ba5108c72739f84923d46c455a9e4bb939
Justin <justinminh225@gmail.com>
2016-06-23 13:45:03 -0700

    Video player dynamically render video title

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,10 +1,10 @@
-var VideoPlayer = () => (
+var VideoPlayer = ({video}) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
       <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
+      <h3>{video.snippet.title}</h3>
       <div>Video Description</div>
     </div>
   </div>
