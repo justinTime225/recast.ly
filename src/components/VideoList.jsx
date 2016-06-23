cf852ac87a77f9b9076de029e57c8b05011f1065
Justin <justinminh225@gmail.com>
2016-06-23 01:40:44 -0700

    Render a single video for each video inside fake data

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,16 +1,14 @@
-var VideoList = () => (
-  <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-  </div>
-);
+var VideoList = ({videos}) => {
+  console.log('hello', videos);
+  return (<div className="video-list media">
+    {videos.map(function(video) {
+      return <VideoListEntry
+        key={video.etag}
+        video={video}
+      />;
+    })}
+  </div>);
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
